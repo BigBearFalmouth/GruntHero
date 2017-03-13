745f0ca4002fe5d1659104482f25ad1f4c64ad40
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2017-03-13 12:43:04 +0000

    First Check in

---
+++ Assets/MyAssets/Scripts/AIMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class AIMovement : MonoBehaviour {
+
+    public AICharacterControl selectedCharacterController;
+    public Transform Target;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray,out hit))
+            {
+                Target.position = hit.point;
+                selectedCharacterController.SetTarget(Target);
+            }
+        }
+	}
+}
